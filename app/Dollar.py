aabd60428fa0f90e6f42fde820f6ee0aba67625f
Firzar Ayupov <f.aupov@vantaai.ru>
2025-03-16 13:33:09 +0300

    chapter 4

--- app/Dollar.py
+++ app/Dollar.py
@@ -1,12 +1,12 @@
 class Dollar:
 
     def __init__(self, amount: int):
-        self.amount = amount
+        self.__amount = amount
 
     def times(self, multiplier: int):
-        return Dollar(self.amount * multiplier)
+        return Dollar(self.__amount * multiplier)
 
     def __eq__(self, other):
         if not isinstance(other, Dollar):
             return False
-        return self.amount == other.amount
+        return self.__amount == other.__amount
